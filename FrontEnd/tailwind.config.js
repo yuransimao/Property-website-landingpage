349b120817ecd5e32bf794bb5a6bd8d462c2bff2
yuransimao <yuransimao7@gmail.com>
2024-02-01 11:46:02 +0100

    Adding Tailwind and removed the initial template

---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Montserrat': ['Montserrat', 'sans-serif'],
+        'OpenSans': ['Open Sans', 'sans-serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
