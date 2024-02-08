e88e79bc737b88a4ba9e55531b7369594399f3ac
yuransimao <yuransimao7@gmail.com>
2024-02-08 11:02:57 +0100

    update

--- FrontEnd/src/components/inputs/inputs.jsx
+++ FrontEnd/src/components/inputs/inputs.jsx
@@ -5,14 +5,14 @@ function Inputs({ type, name, id, labelName, icon }) {
 
     <React.Fragment>
       <div className="flex flex-col relative group py-2 mb-9">
-        <label htmlFor={id} className={`absolute top-0 transition-all duration-300 group-focus-within:top-[-1rem]  flex gap-2 items-center justify-center`}>
+        <label htmlFor={id} className={`absolute top-0 transition-all duration-300 group-focus-within:top-[-1rem]   flex gap-2 items-center justify-center`}>
 
           <span className={`bg-zinc-900/10 p-2 rounded-full group-focus-within:bg-blue-900 font-bold group-focus-within:text-white`}>{icon}
           </span>
           <h3 className='group-focus-within:text-blue-800'>{labelName}</h3>
         </label>
 
-        <input type={type} name={name} id={id} className="border-blue-400 border-solid border-b-2 pt-2 pl-2 bg-transparent focus:border-blue-700 capitalize" autoComplete='on'/>
+        <input type={type} name={name} id={id} className="border-blue-400 border-solid border-b-2 pt-2 pl-2 bg-transparent focus:border-blue-700 capitalize " autoComplete='on'/>
       </div>
     </React.Fragment>
   )
--- FrontEnd/src/components/mordalbox/mordalbox.jsx
+++ FrontEnd/src/components/mordalbox/mordalbox.jsx
@@ -5,16 +5,8 @@ import PropTypes from 'prop-types'
 function Mordalbox({children}) {
   return (
     <React.Fragment>
-        <div className='fixed flex items-center justify-center h-dvh w-dvw z-[60] bg-zinc-700/60 overflow-hidden'>
-            <div className='bg-white rounded-sm overflow-hidden w-2/5'>
-            <div className='flex items-center justify-between gap-1'>
-               <button className='py-4 px-20 bg-blue-500 text-white'><h3>Sign in</h3></button>
-               <button className='py-4 px-20'><h2>Register</h2></button>
-            </div>
-           <div>
-           {children}
-           </div>
-            </div>
+        <div className='fixed flex items-center justify-center h-dvh w-dvw z-[60] bg-zinc-700/90'>
+            {children}
            
         </div>
     </React.Fragment>
