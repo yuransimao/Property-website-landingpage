46f0991415527832d67e3fdf9728e33f4e5e5990
yuransimao <yuransimao7@gmail.com>
2024-02-11 09:18:22 +0100

    separating by hooks

---
+++ FrontEnd/src/hooks/useChangeForm.jsx
@@ -0,0 +1,22 @@
+import  { useState } from 'react'
+import { UseSchema } from './useSchema';
+
+
+function UseChangeForm() {
+
+    const { reset } = UseSchema()
+
+    const TextBtnSignin = 'Sign in';
+    const TextBtnSignup = 'Sign up';
+    const [ChangeForms, setChangeForms] = useState(TextBtnSignin);
+
+    const haldleOnclick = (e) => {
+        const buttonText = e.target.textContent;
+        setChangeForms(buttonText)
+        reset()
+    }
+
+    return {TextBtnSignin, TextBtnSignup,ChangeForms, haldleOnclick}
+}
+
+export { UseChangeForm }
